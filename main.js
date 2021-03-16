861b546fbd22ae9344495ae488ad0a62338acf5b
manggong <jihwank1995@gmail.com>
2021-03-16 22:17:50 +0900

    add animation in visual

--- main.js
+++ main.js
@@ -32,3 +32,12 @@ window.addEventListener('scroll', _.throttle(function() {
     })
   }
 }, 300))
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+
+fadeEls.forEach((fadeEl, index)=>{
+  gsap.to(fadeEl, 1, {
+    delay:(index + 1) * 0.7,
+    opacity: 1
+  })
+})
