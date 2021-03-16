6f5b08d6c4f5464ea7cd042f97b2006e7e3e5f9d
manggong <jihwank1995@gmail.com>
2021-03-16 20:30:02 +0900

    add badges animation

--- main.js
+++ main.js
@@ -17,6 +17,18 @@ searchInputEl.addEventListener('blur', function() {
 
 const badgeEl = document.querySelector('header .badges')
 
-window.addEventListener('scroll', function() {
-  
-})
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY)
+  if (window.scrollY > 500) {
+    console.log("hello")
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300))
